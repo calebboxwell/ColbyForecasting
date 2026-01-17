f946ef30f5a6fdf0f49c354e8a8f023da9917fde
Caleb Boxwell <cjboxw26@colby.edu>
2026-01-17 21:52:19 +0000

    Revised my observations.R code with the comments made by @btupper on 2026-01-16

--- functions/observations.R
+++ functions/observations.R
@@ -1,70 +1,58 @@
+#' Read raw OBIS data and then filter it
+#' 
+#' @param scientificname chr, the name of the species to read
+#' @param minimum_year num, the earliest year of observation to accept or 
+#'   set to NULL to skip
+#' @param temporal_filter num vector, defines range of observations
+#' @param remove_missing chr vector, column names to remove from observations list
+#' @param spatial_filter logical, apply spatial mask 
+#' @return a filtered table of observations
+
 read_observations = function(scientificname = "Clupea harengus",
-                             minimum_year = 1970, 
-                             ...){
+                                 minimum_year = 1970,
+                                 temporal_filter = c(1983, 2013),
+                                 remove_missing = c("eventDate", "individualCount"),
+                                 spatial_filter = TRUE,
+                                 ...){
   
-  #' Read raw OBIS data and then filter it
-  #' 
-  #' @param scientificname chr, the name of the species to read
-  #' @param minimum_year num, the earliest year of observation to accept or 
-  #'   set to NULL to skip
-  #' @param ... other arguments passed to `read_obis()`
-  #' @return a filtered table of observations
-  
-  # Happy coding!
-  
-  # read in the raw data
+  # read in raw OBIS data
   x = read_obis(scientificname, ...) |>
     dplyr::mutate(month = factor(month, levels = month.abb))
   
-  # eventDate must exist! Otherwise I can't say how variables affect it in time
-  x = x |>
-    dplyr::filter(!is.na(eventDate))
+  # set a minimum year for observations
+  if (!is.null(minimum_year)) {
+    x <- dplyr::filter(x, year >= minimum_year)
+  }
   
-  # individual count must exist
-  x = x |>
-    dplyr::filter(!is.na(individualCount))
+  # set further temporal constraints to target data
+  if (!is.null(temporal_filter)) {
+    x <- dplyr::filter(
+      x,
+      year >= temporal_filter[1],
+      year <= temporal_filter[2]
+    )
+  }
   
-  # if the user provided a non-NULL filter by year (caleb: I don't fully understand how this works)
-  if (!is.null(minimum_year)){
-    x = x |>
-      filter(year >= minimum_year)
+  # remove observations with missing variables
+  #' missing dates
+  if ("eventDate" %in% remove_missing) {
+    x <- dplyr::filter(x, !is.na(eventDate))
+  }
+  
+  #' missing observation counts
+  if ("individualCount" %in% remove_missing) {
+    x <- dplyr::filter(x, !is.na(individualCount))
   }
   
   # spatial filter
-  db = brickman_database() |>
-    filter(scenario == "STATIC", var == "mask")
-  mask = read_brickman(db)
-  hitOrMiss = extract_brickman(mask, x)
-  x = x |>
-    filter(!is.na(hitOrMiss$value))
-  
-  # temporal filter - to fine tune the range
-  obs = obs |>
-    filter(year >= 1983 & year <= 2013)
-  dim(obs)
-  
-  # plot observation counts per year
-  ggplot(data = obs,
-         mapping = aes(x = year)) + 
-    geom_bar() + 
-    geom_vline(xintercept = c(1983, 2013), linetype = "dashed") + 
-    labs(title = "Counts per year")
-  
-  # plot counts per month
-  obs = obs |>
-    mutate(month = factor(month, levels = month.abb))
-  
-  ggplot(data = obs,
-         mapping = aes(x = month)) + 
-    geom_bar() + 
-    labs(title = "Counts per month")
-  
-  # plot points on mask of the Gulf of Maine
-  db = brickman_database() |>
-    filter(scenario == "STATIC", var == "mask")
-  mask = read_brickman(db)
-  plot(mask, breaks = "equal", axes = TRUE, reset = FALSE)
-  plot(st_geometry(obs), pch = ".", add = TRUE)
+  if (spatial_filter) {
+    db = brickman_database() |>
+      filter(scenario == "STATIC", var == "mask")
+    mask = read_brickman(db)
+    hitOrMiss = extract_brickman(mask, x)
+    x = x |>
+      filter(!is.na(hitOrMiss$value))
+  }
   
   return(x)
 }
